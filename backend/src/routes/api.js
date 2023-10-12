1ed626d94132981494ecd260bcc79cd161a7575d
Kaan <kaanyurdkl@gmail.com>
2023-10-12 12:34:45 -0400

    Create orders router in the backend

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const usersRouter = require("./users/users.router");
 const productsRouter = require("./products/products.router");
+const ordersRouter = require("./orders/orders.router");
 const uploadsRouter = require("./uploads/upload.router");
 
 const api = express.Router();
 
 api.use("/users", usersRouter);
+api.use("/orders", ordersRouter);
 api.use("/products", productsRouter);
 api.use("/uploads", uploadsRouter);
 
---
+++ backend/src/routes/orders/orders.router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const ordersRouter = express.Router();
+
+ordersRouter.get("/", (req, res) => {});
+
+module.exports = ordersRouter;
