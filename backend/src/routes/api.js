9e982828dfa63f6eb102ae6702b168e52096a51d
Kaan <kaanyurdkl@gmail.com>
2023-10-14 14:23:50 -0400

    Paypal first integration

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -5,11 +5,17 @@ const productsRouter = require("./products/products.router");
 const ordersRouter = require("./orders/orders.router");
 const uploadsRouter = require("./uploads/upload.router");
 
+require("dotenv").config();
+
 const api = express.Router();
 
 api.use("/users", usersRouter);
 api.use("/orders", ordersRouter);
 api.use("/products", productsRouter);
 api.use("/uploads", uploadsRouter);
+
+api.get("/config/paypal", (res, req) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
 
 module.exports = api;
--- frontend/src/views/OrderView.vue
+++ frontend/src/views/OrderView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
+import { loadScript } from "@paypal/paypal-js";
 
 import { useOrdersStore } from "@/stores/ordersStore";
 
@@ -10,9 +11,23 @@ const props = defineProps<{
 const ordersStore = useOrdersStore();
 
 const orderDetails = ref(null);
+const paypal = ref(null);
 
 onMounted(async () => {
   orderDetails.value = await ordersStore.getOrderDetails(props.id);
+  paypal.value = await loadScript({
+    clientId: import.meta.env.VITE_PAYPAL_CLIENT_ID,
+  });
+
+  if (paypal.value) {
+    await paypal.value
+      .Buttons({
+        createOrder() {
+          console.log("Create order");
+        },
+      })
+      .render(".paypal-buttons-container");
+  }
 });
 </script>
 
@@ -20,6 +35,7 @@ onMounted(async () => {
   <section class="heading">
     <h2>Order</h2>
   </section>
+  <section class="paypal-buttons-container"></section>
   <section class="order">{{ orderDetails }}</section>
 </template>
 
