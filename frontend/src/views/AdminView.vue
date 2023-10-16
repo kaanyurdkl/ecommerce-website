ac6f80d1c26e40b5c495eb329054ef9a66fd8ec1
Kaan <kaanyurdkl@gmail.com>
2023-10-16 11:25:52 -0400

    Display all products in the admin page

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, computed, onMounted } from "vue";
+import { useProductsStore } from "@/stores/productsStore";
 import { useOrdersStore } from "@/stores/ordersStore";
 
+const productsStore = useProductsStore();
 const ordersStore = useOrdersStore();
 
 const orders = ref(null);
+const products = computed(() => productsStore.getAllProducts);
 
 const deliverOrderHandler = async (id) => {
   await ordersStore.deliverOrder(id);
   orders.value = await ordersStore.getAllOrders();
+};
+
+const deleteProductHandler = async (id) => {
+  console.log(id);
 };
 
 onMounted(async () => {
@@ -20,9 +27,19 @@ onMounted(async () => {
   <section class="heading">
     <h2>Admin</h2>
   </section>
-  <section class="orders">
-    {{ orders }}
+  <section>
+    <h3>Orders</h3>
+    <p>
+      {{ orders }}
+    </p>
     <button @click="deliverOrderHandler(orders[0]._id)">Deliver</button>
+  </section>
+  <section>
+    <h3>Products</h3>
+    <p>
+      {{ products }}
+    </p>
+    <button @click="deleteProductHandler(products[0]._id)">Delete</button>
   </section>
 </template>
 
