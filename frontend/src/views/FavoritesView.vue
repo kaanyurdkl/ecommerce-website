f80042c11711a86bf53d8fab758ce3dec093c128
Kaan <kaanyurdkl@gmail.com>
2023-11-06 14:29:33 -0500

    Add header in the empty favorites

--- frontend/src/views/FavoritesView.vue
+++ frontend/src/views/FavoritesView.vue
@@ -12,11 +12,28 @@ const store = useProductsStore();
     heading="Favorites"
     :products="store.getFavoriteProductsReversed"
   />
+  <section v-if="!store.favoriteProducts.length" class="heading">
+    <h2>Favorites</h2>
+  </section>
   <Empty
-    v-else
+    v-if="!store.favoriteProducts.length"
     :heading="'No Favorites Yet!'"
     :message="'Add favorite items'"
     :route-to="'home'"
     :route-label="'Start Shopping'"
   />
 </template>
+<style lang="scss" scoped>
+.heading {
+  padding: 2rem;
+  font-size: 2rem;
+  text-transform: uppercase;
+  @media screen and (max-width: 768px) {
+    text-align: center;
+  }
+  @media screen and (max-width: 425px) {
+    font-size: 1.2rem;
+    text-align: center;
+  }
+}
+</style>
